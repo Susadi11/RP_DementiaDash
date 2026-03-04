a4c7ebea9531dbf3bd63bdee06f0b2cc6f8be7b5
susadi <susadisandanima@gmail.com>
2026-03-04 18:27:38 +0530

    UI changes

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -3,21 +3,21 @@ import Sidebar from './Sidebar';
 
 const Layout = ({ children }) => {
   return (
-    <div className="min-h-screen bg-secondaryBg flex flex-col">
+    <div className="min-h-screen flex flex-col">
       {/* Fixed Navbar */}
       <div className="fixed top-0 left-0 right-0 z-50">
         <Navbar />
       </div>
-      
+
       {/* Main container below navbar */}
-      <div className="flex pt-[73px]">
+      <div className="flex pt-16">
         {/* Fixed Sidebar */}
-        <div className="fixed left-0 top-[73px] bottom-0 z-40">
+        <div className="fixed left-0 top-16 bottom-0 z-40">
           <Sidebar />
         </div>
-        
+
         {/* Scrollable main content - offset by sidebar width */}
-        <main className="flex-1 ml-64 p-8 min-h-[calc(100vh-73px)] overflow-y-auto">
+        <main className="flex-1 ml-[260px] p-8 min-h-[calc(100vh-64px)] overflow-y-auto">
           {children}
         </main>
       </div>
